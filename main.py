4b3aa789473d800e694b1cf6fb2449fdc8aaa641
Lenovo <gkckdtn126@unist.ac.kr>
2021-08-17 11:20:42 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from urllib import request
+
+from bs4 import BeautifulSoup
+
+target = request.urlopen("http://www.kma.go.kr/weather/forecast/mid-term-rss3.jsp?stnId=108")
+soup = BeautifulSoup(target, "html.parser") #html을 분석하겠다
+#객체를 리턴 한다.
+for location in soup.select("location"):
+    print("도시 :",location.select_one("city").string)
+    print("날씨 :",location.select_one("wf").string)
